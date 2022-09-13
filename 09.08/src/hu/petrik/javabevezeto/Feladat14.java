2be39001552cab782e4ccae90cbbba6e9e980711
John Doe <johndoe@example.com>
2022-09-13 16:13:21 +0200

    Kész

--- 09.08/src/hu/petrik/javabevezeto/Feladat14.java
+++ 09.08/src/hu/petrik/javabevezeto/Feladat14.java
@@ -1,6 +1,5 @@
 package hu.petrik.javabevezeto;
 
-import java.text.ParseException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,7 +10,7 @@ public class Feladat14 {
         System.out.print("Kérek egy szót:");
 
         String szo = sc.nextLine();
-        String csinal= "";
+        String csinal = "";
         /*do {
             System.out.print("Mit szeretnél csinálni?");
             csinal = sc.nextLine();
@@ -30,16 +29,52 @@ public class Feladat14 {
 
         } while (csinal != "f");*/
 
-        while(!csinal.equals("f")){
+        while (!csinal.equals("f")) {
 
-            System.out.print("Mit szeretnél csinálni?");
+            System.out.print("Mit szeretnél csinálni?\n");
             System.out.println("\ta - Nagy");
             System.out.println("\tb - Kicsi");
             System.out.println("\tc - Hossz");
             System.out.println("\td - Összehasonlít");
             System.out.println("\te - Kiiratás");
             System.out.println("\tf - Kilép");
-            csinal = sc.nextLine();
+            csinal = sc.nextLine().toLowerCase();
+
+            if (csinal.equals("a")) {
+                szo = szo.toUpperCase(Locale.ROOT);
+                System.out.println(szo);
+            } else if (csinal.equals("b")) {
+                szo = szo.toLowerCase(Locale.ROOT);
+                System.out.println(szo);
+            } else if (csinal.equals("c")) {
+                System.out.println("A szöveg hossza: " + szo.length());
+            } else if (csinal.equals("d")) {
+                System.out.println("Kérem a másik szót!");
+
+                String szo2 = sc.nextLine();
+
+                int osszehasonlit = szo.toUpperCase(Locale.ROOT).compareTo(szo2.toLowerCase(Locale.ROOT));
+
+                if (osszehasonlit < 0) {
+                    System.out.println("Az első van előrébb!");
+                } else if (osszehasonlit > 0) {
+                    System.out.println("A második szöveg van előrébb!");
+                } else {
+                    System.out.println("A két szöveg megegyezik!");
+                }
+
+            } else if (csinal.equals("e")) {
+                System.out.println("Mettől?");
+                int a = sc.nextInt();
+                System.out.println("Meddig?");
+                int b = sc.nextInt();
+
+                System.out.println(szo.substring(a - 1, b));
+            } else if (csinal.equals("f")) {
+                System.out.println("Kilépés...");
+            } else {
+                System.out.println("Hiba");
+            }
 
         }
 
