1460b3d1d8e9a243dfa2870001ad459c93c4b2cb
John Doe <johndoe@example.com>
2022-09-13 15:36:35 +0200

    bekérés

--- 09.08/src/hu/petrik/javabevezeto/Feladat14.java
+++ 09.08/src/hu/petrik/javabevezeto/Feladat14.java
@@ -1,13 +1,34 @@
 package hu.petrik.javabevezeto;
 
 import java.text.ParseException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Feladat14 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Kérek egy szót:");
+        System.out.print("Kérek egy szót:");
+
+        String szo = sc.nextLine();
+        String csinal;
+        do {
+            System.out.print("Mit szeretnél csinálni?");
+            csinal = sc.nextLine();
+
+            if (csinal == "a") {
+                szo = szo.toUpperCase(Locale.ROOT);
+                System.out.println(szo);
+            }
+            if (csinal == "b") {
+                szo = szo.toLowerCase(Locale.ROOT);
+                System.out.println(szo);
+            }
+            if (csinal == "c"){
+                System.out.println(szo.length());
+            }
+
+        } while (csinal != "e");
 
     }
 }
