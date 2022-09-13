9b5ca3b234d0d5cb90d40ba10ed68fc6ec7701b6
John Doe <johndoe@example.com>
2022-09-13 15:44:09 +0200

    menü

--- 09.08/src/hu/petrik/javabevezeto/Feladat14.java
+++ 09.08/src/hu/petrik/javabevezeto/Feladat14.java
@@ -11,24 +11,37 @@ public class Feladat14 {
         System.out.print("Kérek egy szót:");
 
         String szo = sc.nextLine();
-        String csinal;
-        do {
+        String csinal= "";
+        /*do {
             System.out.print("Mit szeretnél csinálni?");
             csinal = sc.nextLine();
 
             if (csinal == "a") {
                 szo = szo.toUpperCase(Locale.ROOT);
                 System.out.println(szo);
-            }
-            if (csinal == "b") {
+            } else if (csinal == "b") {
                 szo = szo.toLowerCase(Locale.ROOT);
                 System.out.println(szo);
-            }
-            if (csinal == "c"){
+            } else if (csinal == "c") {
                 System.out.println(szo.length());
+            } else if () {
+
             }
 
-        } while (csinal != "e");
+        } while (csinal != "f");*/
+
+        while(!csinal.equals("f")){
+
+            System.out.print("Mit szeretnél csinálni?");
+            System.out.println("\ta - Nagy");
+            System.out.println("\tb - Kicsi");
+            System.out.println("\tc - Hossz");
+            System.out.println("\td - Összehasonlít");
+            System.out.println("\te - Kiiratás");
+            System.out.println("\tf - Kilép");
+            csinal = sc.nextLine();
+
+        }
 
     }
 }
