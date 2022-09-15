ef15b07cd90d593f4ba42ae00b209599c05d0c78
John Doe <johndoe@example.com>
2022-09-15 09:18:37 +0200

    10 véletlen

--- 09.15/src/hu/petrik/pontoop/Main.java
+++ 09.15/src/hu/petrik/pontoop/Main.java
@@ -7,9 +7,21 @@ public class Main {
     Pont p1=new Pont();
     Pont p2=new Pont(0,0);
     Pont p3 = new Pont(3,4);
+    Pont p4 = new Pont(100);
 
-        System.out.println(p1);
+        /*System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
+        System.out.println(p4);*/
+
+        Pont[] pontok= new Pont[10];
+
+        for (int i = 0; i < pontok.length; i++) {
+            pontok[i] = new Pont(100);
+        }
+
+        for (Pont p: pontok){
+            System.out.println(p);
+        }
     }
 }
