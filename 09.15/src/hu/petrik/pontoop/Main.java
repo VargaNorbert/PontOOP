b5874d70fc2bd113a9a10e83a3802964ad9aefad
Varga Norbert <vnorbi02@gmail.com>
2022-09-15 10:24:47 +0200

    Két pont távolsága

--- 09.15/src/hu/petrik/pontoop/Main.java
+++ 09.15/src/hu/petrik/pontoop/Main.java
@@ -5,7 +5,7 @@ import hu.petrik.pontoop.pont.Pont;
 public class Main {
     public static void main(String[] args) {
     Pont p1=new Pont();
-    Pont p2=new Pont(0,0);
+    Pont p2=new Pont(1,1);
     Pont p3 = new Pont(3,4);
     Pont p4 = new Pont(100);
 
@@ -23,5 +23,18 @@ public class Main {
         for (Pont p: pontok){
             System.out.println(p);
         }
+
+        int legtavolabbiPontIndex=0;
+        for (int i = 1; i < pontok.length; i++) {
+            if (pontok[legtavolabbiPontIndex].getOrigotolMertTavolsag()<pontok[i].getOrigotolMertTavolsag()){
+                legtavolabbiPontIndex=i;
+            }
+        }
+
+        System.out.printf("A legtávolabbi pont az origótól a(z) %d. pont, koordinátái:%s, origótól mért távolsága: %.3f\n",
+                (legtavolabbiPontIndex+1),pontok[legtavolabbiPontIndex],pontok[legtavolabbiPontIndex].getOrigotolMertTavolsag());
+
+        System.out.println(p1.getKetPontTavolsaga(p2));
+
     }
 }
